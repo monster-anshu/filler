2c715ef334c637778bf9fb92f375fd5128c8c3ea
Himanshu Gunwant <hgunwant2312@gmail.com>
2024-07-13 17:13:02 +0530

    update template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const templates: Template[] = [
     },
     template: `I am writing to express my interest in the {{job.role}} role at {{company.name}}. With a strong background in JavaScript and TypeScript, along with a passion for developing efficient and scalable web applications, I am excited about the opportunity to contribute to {{company.name}}'s mission.
 
-I have extensive experience working with modern frameworks and libraries, particularly React.js, Next.js, Node.Js, MongoDB, Redux, and have successfully delivered projects that meet user needs. Recently, I developed a booking application featuring a robust scheduling system and implemented a serverless notification system on AWS. These experiences have strengthened my ability to create high-quality software solutions.
+I have extensive experience working with modern frameworks and libraries, particularly React.js, Next.js, Node.Js, MongoDB, Redux and have successfully delivered projects that meet user needs. Recently, I developed a booking application featuring a robust scheduling system and implemented a serverless notification system on AWS. These experiences have strengthened my ability to create high-quality software solutions.
   
 I would love the opportunity to discuss how my skills and experiences align with the goals of {{company.name}}. Thank you for considering my application. I look forward to the possibility of working together.`,
   },
